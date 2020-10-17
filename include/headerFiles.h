80ff314977780f9f861d3066799c3401455afb16
Rupal Jain <rupaljain9727@gmail.com>
2020-10-17 15:11:50 +0530

    Header Files and Global Functions are added

---
+++ include/headerFiles.h
@@ -0,0 +1,80 @@
+/// Header Files
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#ifndef HEADERFILES_H
+#define HEADERFILES_H
+
+/// Solution
+vector<vector<char>> SolvingByUser(vector<vector<char>> vec);
+vector<vector<char>> SolvingByAlgorithm(vector<vector<char>> vec);
+
+
+/// Printing the Cube
+vector<vector<char>> PrintingCube(vector<vector<char>> vec);
+
+/// Time Delay
+void delay(unsigned int mseconds);
+
+/// RED Moves
+vector<vector<char>> Red_Right_Clock(vector<vector<char>> vec);
+vector<vector<char>> Red_Right_Anti_Clock(vector<vector<char>> vec);
+vector<vector<char>> Red_Left_Clock(vector<vector<char>> vec);
+vector<vector<char>> Red_Left_Anti_Clock(vector<vector<char>> vec);
+vector<vector<char>> Red_Up_Clock(vector<vector<char>> vec);
+vector<vector<char>> Red_Up_Anti_Clock(vector<vector<char>> vec);
+vector<vector<char>> Red_Down_Clock(vector<vector<char>> vec);
+vector<vector<char>> Red_Down_Anti_Clock(vector<vector<char>> vec);
+vector<vector<char>> Red_Front_Clock(vector<vector<char>> vec);
+vector<vector<char>> Red_Front_Anti_Clock(vector<vector<char>> vec);
+vector<vector<char>> Red_Back_Clock(vector<vector<char>> vec);
+vector<vector<char>> Red_Back_Anti_Clock(vector<vector<char>> vec);
+
+
+/// BLUE Moves
+vector<vector<char>> Blue_Right_Clock(vector<vector<char>> vec);
+vector<vector<char>> Blue_Right_Anti_Clock(vector<vector<char>> vec);
+vector<vector<char>> Blue_Left_Clock(vector<vector<char>> vec);
+vector<vector<char>> Blue_Left_Anti_Clock(vector<vector<char>> vec);
+vector<vector<char>> Blue_Up_Clock(vector<vector<char>> vec);
+vector<vector<char>> Blue_Up_Anti_Clock(vector<vector<char>> vec);
+vector<vector<char>> Blue_Down_Clock(vector<vector<char>> vec);
+vector<vector<char>> Blue_Down_Anti_Clock(vector<vector<char>> vec);
+vector<vector<char>> Blue_Front_Clock(vector<vector<char>> vec);
+vector<vector<char>> Blue_Front_Anti_Clock(vector<vector<char>> vec);
+vector<vector<char>> Blue_Back_Clock(vector<vector<char>> vec);
+vector<vector<char>> Blue_Back_Anti_Clock(vector<vector<char>> vec);
+
+
+/// ORANGE Moves
+vector<vector<char>> Orange_Right_Clock(vector<vector<char>> vec);
+vector<vector<char>> Orange_Right_Anti_Clock(vector<vector<char>> vec);
+vector<vector<char>> Orange_Left_Clock(vector<vector<char>> vec);
+vector<vector<char>> Orange_Left_Anti_Clock(vector<vector<char>> vec);
+vector<vector<char>> Orange_Up_Clock(vector<vector<char>> vec);
+vector<vector<char>> Orange_Up_Anti_Clock(vector<vector<char>> vec);
+vector<vector<char>> Orange_Down_Clock(vector<vector<char>> vec);
+vector<vector<char>> Orange_Down_Anti_Clock(vector<vector<char>> vec);
+vector<vector<char>> Orange_Front_Clock(vector<vector<char>> vec);
+vector<vector<char>> Orange_Front_Anti_Clock(vector<vector<char>> vec);
+vector<vector<char>> Orange_Back_Clock(vector<vector<char>> vec);
+vector<vector<char>> Orange_Back_Anti_Clock(vector<vector<char>> vec);
+
+
+/// GREEN Moves
+vector<vector<char>> Green_Right_Clock(vector<vector<char>> vec);
+vector<vector<char>> Green_Right_Anti_Clock(vector<vector<char>> vec);
+vector<vector<char>> Green_Left_Clock(vector<vector<char>> vec);
+vector<vector<char>> Green_Left_Anti_Clock(vector<vector<char>> vec);
+vector<vector<char>> Green_Up_Clock(vector<vector<char>> vec);
+vector<vector<char>> Green_Up_Anti_Clock(vector<vector<char>> vec);
+vector<vector<char>> Green_Down_Clock(vector<vector<char>> vec);
+vector<vector<char>> Green_Down_Anti_Clock(vector<vector<char>> vec);
+vector<vector<char>> Green_Front_Clock(vector<vector<char>> vec);
+vector<vector<char>> Green_Front_Anti_Clock(vector<vector<char>> vec);
+vector<vector<char>> Green_Back_Clock(vector<vector<char>> vec);
+vector<vector<char>> Green_Back_Anti_Clock(vector<vector<char>> vec);
+
+
+#endif
